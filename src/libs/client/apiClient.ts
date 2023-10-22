6049e8de5b88bf897a2b468275ac91ff2b4a38dd
aliraza2u <raza8r@gmail.com>
2023-10-22 06:26:48 +0500

    write axios instance

--- src/libs/client/apiClient.ts
+++ src/libs/client/apiClient.ts
@@ -1,129 +1,37 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios from 'axios';
 
-// Create an Axios instance with custom configuration
-const apiInstance: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:4000/rest', // Set your API base URL
-  timeout: 10000, // Set the default timeout
+const apiClient = axios.create({
+  baseURL: 'http://localhost:4000/rest',
+  timeout: 30000,
+  withCredentials: true,
   headers: {
-    'Content-Type': 'application/json' // Set default content type
-  },
-  withCredentials: true // Set default withCredentials to true
+    'Content-Type': 'application/json'
+  }
 });
 
-// Define Request Config with generic types
-interface RequestConfig<T> {
-  data?: T;
-  headers?: Record<string, string>;
-  params?: Record<string, string>;
-  timeout?: number;
-  withCredentials?: boolean;
+export const { get, post, put, delete: destroy } = apiClient;
+export default apiClient;
 
-  // Add more request-specific configuration options as needed
-}
+// how to use it
 
-// Define Response Schema with generic types
-interface ApiResponse<T> {
-  data: T;
-  status: number;
-  statusText: string;
-  headers: Record<string, string>;
-  config: AxiosRequestConfig;
-  request?: any;
-}
+// const getLeads = async () => {
+//   const { data } = await get('/lead');
+//   return data;
+// };
 
-// Request Interceptor
-apiInstance.interceptors.request.use(
-  (config) => {
-    // Modify request config here (e.g., add headers, authentication tokens)
-    return config;
-  },
-  (error) => {
-    // Handle request error (e.g., network issues)
-    return Promise.reject(error);
-  }
-);
+// const createLead = async (lead) => {
+//   const { data } = await post('/lead', lead);
+//   return data;
+// };
 
-// Response Interceptor
-apiInstance.interceptors.response.use(
-  (response) => {
-    // Modify response data or headers here if needed
-    return response;
-  },
-  (error) => {
-    // Handle response errors (e.g., HTTP error codes, custom error handling)
-    return Promise.reject(error);
-  }
-);
+// const updateLead = async (lead) => {
+//   const { data } = await put('/lead', lead);
+//   return data;
+// };
 
-// Usage example: Making a GET request with cancellation using AbortController
-function fetchData<T>(endpoint: string, config?: RequestConfig<T>) {
-  const controller = new AbortController();
-  const signal = controller.signal;
+// const deleteLead = async (lead) => {
+//   const { data } = await destroy('/lead', lead);
+//   return data;
+// };
 
-  const requestConfig: AxiosRequestConfig = {
-    ...config,
-    signal // Attach the AbortController's signal to the request
-  };
-
-  return apiInstance.get(endpoint, requestConfig).finally(() => {
-    controller.abort(); // Cancel the request when it's no longer needed
-  });
-}
-
-// Usage example: Making a POST request with cancellation using AbortController
-function postData<T>(endpoint: string, config: RequestConfig<T>) {
-  debugger;
-
-  const controller = new AbortController();
-  const signal = controller.signal;
-
-  const requestConfig: AxiosRequestConfig = {
-    ...config.data,
-    signal // Attach the AbortController's signal to the request
-  };
-  //   const requestConfig: AxiosRequestConfig = {
-  //     method: 'POST',
-  //     url: 'https://your-api-endpoint.com/your-post-route',
-  //     data: requestData,
-  //     signal
-
-  //     // Add headers, authorization, or other request configuration options as needed
-  //   };
-
-  return apiInstance
-    .post('http://localhost:4000/rest/auth/login', requestConfig)
-    .then((res) => {
-      debugger;
-      console.log(res);
-    })
-    .catch((e) => console.log(e))
-    .finally(() => {
-      controller.abort(); // Cancel the request when it's no longer needed
-    });
-}
-
-export { fetchData, postData };
-
-// async function performPostRequest() {
-//   const requestData = {
-//     key1: 'value1',
-//     key2: 'value2'
-//     // Add more data as needed
-//   };
-
-//   const requestConfig: AxiosRequestConfig = {
-//     method: 'POST',
-//     url: 'https://your-api-endpoint.com/your-post-route',
-//     data: requestData
-//     // Add headers, authorization, or other request configuration options as needed
-//   };
-
-//   try {
-//     const response: AxiosResponse = await axios(requestConfig);
-//     console.log('POST request successful', response.data);
-//   } catch (error) {
-//     console.error('POST request failed', error);
-//   }
-// }
-
-// performPostRequest();
+// export { getLeads, createLead, updateLead, deleteLead };
--- src/redux/middleware/lead.ts
+++ src/redux/middleware/lead.ts
@@ -1,27 +1,51 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-import createAbortController from '../../utils/createAbortController';
+import { get, post, put, destroy } from '../../libs/client/apiClient';
+import { LeadsTypes } from '../../types';
 
-export const getLeads = createAsyncThunk('mySlice/myAsyncThunk', async ({ signal }: { signal: AbortSignal }) => {
-  const { abort } = createAbortController();
-
-  const config = {
-    signal
-    // Other Axios configurations
-  };
-
+export const getLeads = createAsyncThunk('lead/get', async ({ signal }: { signal: AbortSignal }) => {
   try {
-    const { data } = await axios.get('http://localhost:4000/rest/lead', config);
-    // store the abort function in the state so we can cancel the request if needed
-
+    const { data } = await get('/lead', { signal });
     return data.data;
   } catch (error) {
-    if (error.name === 'AbortError') {
-      // Handle cancellation
-      abort();
-    } else {
-      // Handle other errors
+    throw error;
+  }
+});
+
+export const createLead = createAsyncThunk('lead/create', async ({ lead, signal }: { lead: LeadsTypes; signal: AbortSignal }) => {
+  try {
+    const { data } = await post('/lead', lead, { signal });
+    return data.data;
+  } catch (error) {
+    throw error;
+  }
+});
+
+export const createBulkLead = createAsyncThunk(
+  'lead/createBulk',
+  async ({ leads, signal }: { leads: LeadsTypes[]; signal: AbortSignal }) => {
+    try {
+      const { data } = await post('/lead/bulk', leads, { signal });
+      return data.data;
+    } catch (error) {
       throw error;
     }
+  }
+);
+
+export const updateLead = createAsyncThunk('lead/update', async ({ lead, signal }: { lead: LeadsTypes; signal: AbortSignal }) => {
+  try {
+    const { data } = await put('/lead', lead, { signal });
+    return data.data;
+  } catch (error) {
+    throw error;
+  }
+});
+
+export const deleteLead = createAsyncThunk('lead/delete', async ({ id }: { id: string }) => {
+  try {
+    const { data } = await destroy(`/lead/${id}`);
+    return data.data;
+  } catch (error) {
+    throw error;
   }
 });
