0f1f0a56d7a395887ceb5720ad1fa456a6b39323
aliraza2u <raza8r@gmail.com>
2023-12-20 19:54:20 +0500

    set dev baseURL

--- src/libs/client/apiClient.ts
+++ src/libs/client/apiClient.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { urls } from '../../apiConfig';
 
-const env = process.env.REACT_APP_STAGE || 'local';
-export const baseURL = urls[env];
+const env = process.env.REACT_APP_STAGE || 'development';
+export const baseURL = 'https://recrm-dd33eadabf10.herokuapp.com/rest';
 
 const apiClient = axios.create({
-  baseURL: baseURL || 'https://recrm-dd33eadabf10.herokuapp.com/rest',
+  baseURL: 'https://recrm-dd33eadabf10.herokuapp.com/rest',
   timeout: 30000,
   withCredentials: true,
   headers: {
